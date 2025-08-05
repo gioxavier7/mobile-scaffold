59f61a8e3a9d14291c20e02047484a94b5448106
GIOVANNA SOARES XAVIER <24122285@dsn127m1246728.cfp127aluno.local>
2025-08-05 12:27:22 -0300

    adicionando lista de clientes

--- app/src/main/java/com/example/clientesapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/clientesapp/screens/HomeScreen.kt
@@ -6,17 +6,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -29,9 +38,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,8 +68,66 @@ fun HomeScreen(modifier: Modifier = Modifier){
                 .fillMaxSize()
                 .background(color = MaterialTheme.colorScheme.background)
         ) {
-            Text(text = "aqui fica o conteudo")
+            Row { 
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "icone da lista de clientes",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Lista de Clientes"
+                )
+            }
+            LazyColumn {
+                items(10){
+                    ClientCard()
+                }
+            }
+
         }
+    }
+}
+
+@Composable
+fun ClientCard(modifier: Modifier = Modifier){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 4.dp
+            ),
+        colors = CardDefaults.cardColors(
+            contentColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Column {
+                Text(text = "Nome do Cliente", fontWeight = FontWeight.Bold)
+                Text(text = "cliente@gmail.com", fontSize = 12.sp)
+            }
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Deletar"
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ClientCardPreview() {
+    ClientesappTheme {
+        ClientCard()
     }
 }
 
